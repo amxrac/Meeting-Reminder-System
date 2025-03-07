38ab3828405b1319d5a20e2a0d741da8ab545bfd
an <108365444+amxrac@users.noreply.github.com>
2025-03-07 05:45:19 +0100

    add appdbcontext and configure sqlserver in program.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Meeting_Reminder_System.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace Meeting_Reminder_System.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<NotificationLog> NotificationLogs { get; set; }
+        public DbSet<UserNotificationPreference> userNotificationPreferences { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
+using Meeting_Reminder_System.Data;
+using Meeting_Reminder_System.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("MRSDB");
+
 
 // Add services to the container.
 
+builder.Services.AddDataProtection();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+    options =>
+    {
+        options.Password.RequiredLength = 8;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+
+    }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
